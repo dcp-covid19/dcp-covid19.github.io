8023423dc99eebe8c4c05488e8cd888ce78e6241
KCErb <kc@kcerb.com>
2020-03-28 08:46:51 -0400

    make org-theme normal css and put in public

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './org-theme.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
