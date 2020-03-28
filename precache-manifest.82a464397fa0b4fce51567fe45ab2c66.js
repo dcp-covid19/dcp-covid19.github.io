d0106d063f51084cea277994101569dce747e252
KCErb <kc@kcerb.com>
2020-03-28 08:16:26 -0400

    Updates

---
+++ precache-manifest.82a464397fa0b4fce51567fe45ab2c66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9507c4476118b3202340cf84a6942168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d834617dcb5e8445f9",
+    "url": "/static/css/main.3cb54ea7.chunk.css"
+  },
+  {
+    "revision": "82358a3d9d9425e95fc1",
+    "url": "/static/js/2.527bda6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527bda6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d834617dcb5e8445f9",
+    "url": "/static/js/main.6e548c77.chunk.js"
+  },
+  {
+    "revision": "a51bdce1b2dc413c1162",
+    "url": "/static/js/runtime-main.b34440fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
