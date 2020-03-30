817f0cf2d0fc6f7cb1d677186d9cf8b2ffc4ebc8
Ryan Rossiter <nayr0788@gmail.com>
2020-03-30 09:47:32 -0400

    Fix header nav items to have background

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import DCPLogo from './DCPLogo';
 
 const LINKS = [
@@ -9,22 +9,24 @@ const LINKS = [
 
 const Header = ({ activeLink, hideDivider=false }) => {
   const [navOpen, setNavOpen] = useState(false);
-  const toggleNav = () => {
-    setNavOpen(!navOpen)
-  }
+
+  const toggleNav = useCallback(() => {
+    setNavOpen((open) => !open);
+  }, [setNavOpen]);
+
   return (
     <header className="main-header">
-      <nav class="navbar navbar-expand-lg fixed-top">
+      <nav className="navbar navbar-expand-lg fixed-top">
         <DCPLogo className=""/>
-        <button class="navbar-toggler" onClick={toggleNav} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" onClick={toggleNav} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class={"collapse navbar-collapse " + (navOpen ? "show" : "")} id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
+        <div className={`collapse navbar-collapse ${navOpen ? "show" : ""}`} id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
             { LINKS.map(({ label, href }) => (
-              <li className="nav-item">
-                <a key={label} href={href} className={`nav-link header-link ${activeLink === label? 'active' : ''}`}>{ label }</a>
+              <li key={label} className="nav-item">
+                <a href={href} className={`nav-link header-link ${activeLink === label? 'active' : ''}`}>{ label }</a>
               </li>
             ))}
           </ul>
